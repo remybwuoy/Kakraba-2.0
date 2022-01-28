fe438081fee611adef92390f9e29f2bc834a752b
Fred Awumey <fredawumeyfafa@gmail.com>
2022-01-28 18:53:21 +0000

    js code modified

--- js/index.js
+++ js/index.js
@@ -1,17 +1,14 @@
-var myModal = document.getElementById('myModal')
-var myInput = document.getElementById('myInput')
-
-myModal.addEventListener('shown.bs.modal', function () {
-  myInput.focus()
-})
 
 var form = document.getElementById('db-form');
   form.addEventListener("submit", e => {
    var button = document.getElementById("submitBtn");
-    button.innerText = 'Submitting';
-    button.disabled = true;
-    button.style.backgroundColor = "#adadad";
-
+    // button.innerText = 'Submitting';
+    // button.disabled = true;
+    // button.style.backgroundColor = "#adadad";
+    $(document).on('submit', 'form', function(e) {
+      $(this).find('button[type="submit"]')
+          .html(`<i class="fas fa-spinner fa-spin"></i> Submitting`);
+ });
     e.preventDefault();
   
     fetch(form.action, {
@@ -25,9 +22,18 @@ var form = document.getElementById('db-form');
       button.style.backgroundColor = "#6db983";
       button.innerText = 'Join Waitlist';
       button.disabled = false;
+ 
+
       swal("Good job!", "Successful!", "success")
       document.getElementById("db-form").reset();
       // window.location.href = 'https://abc.com';
     });
   });
 
+  var myModal = document.getElementById('myModal')
+  var myInput = document.getElementById('myInput')
+  
+  myModal.addEventListener('shown.bs.modal', function () {
+    myInput.focus()
+  })
+  
